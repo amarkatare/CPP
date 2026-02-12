883ba0de58dedf1cca873dd6f38ea0b6a30332f6
Amar Katare <amarkatare2004@gmail.com>
2026-02-12 12:57:30 +0530

    Updated Vaersion

--- Day1/Class.cpp
+++ Day1/Class.cpp
@@ -8,31 +8,40 @@ class Calculator
 public:
 	int variable1;
 	int variable2;
-	float answer;
-//member Function 
-int addition(int variable1,int variable2)
+Calculator(int v1,int v2){
+ variable1=v1;
+ variable2=v2;
+}
+//member Functions 
+int addition()
 {
 	return variable1+variable2;
 }
-int subtraction(int variable1,int variable2)
+int subtraction()
 {
 	return variable1-variable2;
 }
-int multiplication(int variable1,int variable2)
+int multiplication()
 {
 	return variable1*variable2;
 }
-float division(int variable1,int variable2)
+float division()
 {
 	return (float)variable1/variable2;
 }
 };
 int main(int argc, char const *argv[])
 {
-	Calculator op1;
-	cout<<op1.addition(50,20)<<endl;
-	cout<<op1.subtraction(50,20)<<endl;
-	cout<<op1.multiplication(50,20)<<endl;
-	cout<<op1.division(50,20)<<endl;
+	Calculator op1(50,30);
+	Calculator op2(30,50);
+	cout<<op1.addition()<<endl;
+	cout<<op1.subtraction()<<endl;
+	cout<<op1.multiplication()<<endl;
+	cout<<op1.division()<<endl;
+
+	cout<<op2.multiplication()<<endl;
+	cout<<op2.addition()<<endl;
+	cout<<op2.subtraction()<<endl;
+	cout<<op2.division()<<endl;
 	return 0;
 }
